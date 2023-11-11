04a8f074c5e7f227a88110cfdf1ce4e30eda009f
Kaustubh Gayadhankar <gayadhankarkaustubh@gmail.com>
2023-11-11 11:31:01 -0500

    testing mode

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main(config):
                 model.to(device)
                 model.train(x_train_new, y_train_new, 200)
                 model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])"""
-    model.train(x_train_new, y_train_new, 200)
+    #model.train(x_train_new, y_train_new, 200)
     model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])
     # Second step: with hyperparameters determined in the first run, re-train
     # your model on the original train set.
