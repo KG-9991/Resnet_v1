24aeb8c7b4af3b43532817e822879447006d1672
Kaustubh Gayadhankar <gayadhankarkaustubh@gmail.com>
2023-11-11 06:11:07 -0500

    changed dir

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def configure():
                         help='save the checkpoint when epoch MOD save_interval == 0')
     parser.add_argument("--first_num_filters", type=int, default=16, help='number of classes')
     parser.add_argument("--weight_decay", type=float, default=2e-4, help='weight decay rate')
-    parser.add_argument("--modeldir", type=str, default='model_v1', help='model directory')
+    parser.add_argument("--modeldir", type=str, default='model_version1', help='model directory')
     parser.add_argument("--learning_rate", type=float, default=0.1, help='model directory')
 
     ### YOUR CODE HERE
@@ -43,9 +43,9 @@ def main(config):
     #summary(model, (3, 32, 32))
 
     ### YOUR CODE HERE
-    batch_sizes = [32,64,128,256,512,1024]
-    learning_rates = [0.2,0.1,0.01]
-    weight_decays = [0.1,0.01,0.001,0.0001,0.00001]
+    batch_sizes = [32,128,512]
+    learning_rates = [0.1,0.01]
+    weight_decays = [0.0001,0.00001]
     # First step: use the train_new set and the valid set to choose hyperparameters.
     """for i in batch_sizes:
         for j in learning_rates:
@@ -58,9 +58,9 @@ def main(config):
                 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
                 model.to(device)
                 model.train(x_train_new, y_train_new, 200)
-                model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])"""
+                model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])
     model.train(x_train_new, y_train_new, 200)
-    model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])
+    model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])"""
     # Second step: with hyperparameters determined in the first run, re-train
     # your model on the original train set.
     # model.train(x_train, y_train, 10)
