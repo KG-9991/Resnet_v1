e96ab8a7c46c078e3afbb71af4782914dd97446d
Kaustubh Gayadhankar <gayadhankarkaustubh@gmail.com>
2023-11-14 23:55:37 -0500

    added tests

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main(config):
 
     # Third step: after re-training, test your model on the test set.
     # Report testing accuracy in your hard-copy report.
-    model.test_or_validate(x_test, y_test, [190])
+    model.test_or_validate(x_test, y_test, [170,180,190])
     ### END CODE HERE
 
 if __name__ == "__main__":
